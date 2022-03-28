e15e2403e611560eaedba2038f08d8f3477eccfd
Tedec1 <jaspermesenbrink@gmail.com>
2022-03-28 00:39:01 -0600

    added search bar

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -6,10 +6,11 @@ import lineUp from '../util/lineUp.json'
 const Home = (props) => {
     const [monsters,setMonsters] = useState([])
     const [cardOfDay, setCardOfDay] = useState(null);
+    const [input, setInput] = useState("")
     const populateMonsters = async () => {
         try {
             const data = await callApi({
-                type:"Normal Monster"
+                type:(input || "Normal Monster")
             })
             setMonsters(data);
             console.log(data);
@@ -17,19 +18,27 @@ const Home = (props) => {
             console.error(error);
         }
     }
-
-useEffect(()=>{
-    const offset = new Date("March 28, 2022 12:17:00")
-    const current = new Date()
-    const index = (current.getSeconds() - offset.getSeconds()) % lineUp.length || 4
-    console.log(index);
-    setCardOfDay(monsters[index])
-})
+    const handleInput = (e) => {
+        e.preventDefault()
+        setInput(e.target.value)
+    }
+    useEffect(()=>{
+        const offset = new Date("March 28, 2022 12:17:00")
+        const current = new Date()
+        const index = (current.getSeconds() - offset.getSeconds()) % lineUp.length || 4
+        // console.log(index);
+        // setCardOfDay(monsters[index])
+    })
   return (
     <div {...props} id="home">
-        <button onClick={() => populateMonsters()}>
-            click me
-        </button>
+        <div id='form-container'>
+            <div>enter a card type</div>
+            <input onChange={handleInput}>
+            </input>
+            <button onClick={() => populateMonsters()}>
+                submit
+            </button>
+        </div>
             {
                 cardOfDay && <Card key={cardOfDay?.id} {...cardOfDay}/>
             }
