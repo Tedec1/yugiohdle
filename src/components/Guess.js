7811b3f24e4901f530efcdebbfb069c23d1b13a5
Tedec1 <jaspermesenbrink@gmail.com>
2022-07-18 17:13:12 -0600

    fixed autocomplete. starting on game

--- src/components/Guess.js
+++ src/components/Guess.js
@@ -1,16 +1,25 @@
-import React from "react";
+import React, { useContext, useEffect } from "react";
+import { cardOfTheDayContext } from "../pages/Home";
 import "./Guess.css";
-const Guess = ({ cardOfTheDay, monster, ...props }) => {
-    console.log(monster);
-
+const Guess = ({ monster, ...props }) => {
+    // console.log(monster);
+    let cardOfDay = useContext(cardOfTheDayContext);
+    useEffect(() => {
+        console.log(cardOfDay);
+    }, [cardOfDay]);
     return (
         <div className="guess">
             <div className="name">{monster.name}</div>
-            <div className="atk">{monster?.atk || "?"}</div>
-            <div className="def">{monster?.def || "?"}</div>
+            <div className="atk">{monster?.atk}</div>
+            <div className="def">{monster?.def}</div>
             <div className="attr">{monster?.attribute || "?"}</div>
             <div className="lvl">{monster?.level || "?"}</div>
-            {/* <div className="card-sets">{monster.card-sets}</div> */}
+            {/* <div className="card-sets">
+                {monster.card_sets &&
+                    monster.card_sets.map((set) => (
+                        <div className="card-set">{set.set_name}</div>
+                    ))}
+            </div> */}
         </div>
     );
 };
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { Card, Guess } from "../components";
 import { callApi, getNumberOfDays, shuffle } from "../util";
 
@@ -6,75 +6,51 @@ import "./Home.css";
 // import lineUp from "../util/lineUp.json";
 import * as obj from "../util/lineUp.json";
 import { Autocomplete, TextField } from "@mui/material";
-
+export const cardOfTheDayContext = React.createContext({});
 const { cardTypes, lineUp } = obj;
 const Home = (props) => {
     const [monsters, setMonsters] = useState([]);
     const [cardOfDay, setCardOfDay] = useState({});
     const [input, setInput] = useState("");
     const [options, setOptions] = useState([]);
-    // const onSearch = () => {
-    //     return monsters.filter((m) => m?.name.startsWith(input));
-    // };
 
-    const populateMonsters = async (options) => {
+    const getAllMonsters = async () => {
+        // console.log(cardTypes);
         try {
-            // setInput(vaildCardType(input) ? input : "normal monster");
-            // console.log(input);
             const data = await callApi({
-                type: !input.length ? cardTypes.join(",") : input,
+                type: cardTypes.join(","),
             });
-            setMonsters(data);
-            // console.log("28");
             // console.log(data);
+            return data;
         } catch (error) {
             console.error(error);
         }
     };
-    const setAutofillOptions = async () => {
-        setOptions(
-            monsters.reduce((acc, cur) => {
-                acc.push(cur?.name);
-                return acc;
-            }, [])
-        );
-        // console.log(options);
-    };
-    const getAllMonsters = async () => {
-        // console.log(
-        //     cardTypes.slice(0, Math.round(cardTypes.length / 2)).join(",")
-        // );
 
-        // setInput(shuffle(cardTypes).join(","));
-
-        await populateMonsters();
-
-        await setAutofillOptions();
-    };
-
-    const setMonstersToList = () => {
-        setMonsters(lineUp.slice(0, Math.round(lineUp.length / 4)));
-    };
-
-    const handleInput = (e) => {
-        e.preventDefault();
-        setInput(e.target.value);
-    };
     useEffect(() => {
         const setUpMonsters = async () => {
-            await getAllMonsters();
+            console.log("useEffect is being run");
+            const monsterList = await getAllMonsters();
+            const names = monsterList.reduce((acc, cur) => {
+                acc.push(cur?.name);
+                return acc;
+            }, []);
+            setOptions(names);
             const index = getNumberOfDays() % lineUp.length;
             setCardOfDay(await callApi({ id: lineUp[index] }));
-            console.log("cardOfDay :>> ", cardOfDay);
+            console.log("cardOfDay :>> ", cardOfDay[0]);
         };
         setUpMonsters();
     }, []);
+
     return (
         <div {...props} id="home">
             <div id="form-container">
                 <div>Enter a Guess</div>
                 <Autocomplete
                     disablePortal
+                    loading={options.length < 1}
+                    loadingText="Loading"
                     options={options}
                     sx={{ width: 300 }}
                     renderInput={(params) => (
@@ -82,16 +58,28 @@ const Home = (props) => {
                     )}
                 />
                 {/* <input onChange={handleInput}></input> */}
-                <Guess
-                    monster={{
-                        name: "NAME",
-                        atk: "ATK",
-                        def: "DEF",
-                        attribute: "ATTR",
-                        level: "LVL",
-                    }}
-                />
-                <Guess monster={cardOfDay[0]} />
+
+                <cardOfTheDayContext.Provider value={cardOfDay}>
+                    <div className="guess-container">
+                        <Guess
+                            monster={{
+                                name: "NAME",
+                                atk: "ATK",
+                                def: "DEF",
+                                attribute: "ATTR",
+                                level: "LVL",
+                                // card_sets: [{ set_name: "SETS" }],
+                            }}
+                        />
+                        <Guess
+                            monster={
+                                cardOfDay[0]
+                                    ? cardOfDay[0]
+                                    : { name: "loading" }
+                            }
+                        />
+                    </div>
+                </cardOfTheDayContext.Provider>
                 {/* {cardOfDay && cardOfDay.length ? (
                     <Card {...cardOfDay[0]} />
                 ) : null} */}
