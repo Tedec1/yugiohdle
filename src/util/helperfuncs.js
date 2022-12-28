8877d2789bd8ba28709e2c2e0c79774be75c289b
Jasper <jaspermesenbrink@gmail.com>
2022-12-28 11:50:34 -0800

    add midnight to date

--- src/util/helperfuncs.js
+++ src/util/helperfuncs.js
@@ -1,17 +1,17 @@
-export const START_DATE = "6/15/2022";
+export const START_DATE = "6-15-2022T00:00:01";
 export const getNumberOfDays = () => {
     const date1 = new Date(START_DATE);
-    const date2 = new Date();
+    // const date2 = new Date();
 
     // One day in milliseconds
     const oneDay = 1000 * 60 * 60 * 24;
 
     // Calculating the time difference between two dates
-    const diffInTime = date2.getTime() - date1.getTime();
+    const diffInTime = Date.now() - date1.getTime();
 
     // Calculating the no. of days between two dates
     const diffInDays = Math.floor(diffInTime / oneDay);
-    // console.log(diffInDays);
+    console.log(diffInDays);
     return diffInDays;
 };
 
